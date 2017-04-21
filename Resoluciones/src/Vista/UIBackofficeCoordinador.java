e9620cf0b4d30c3c67241f4fca486061eadfbb07
Julian Salinas <julian4001@outlook.com>
2017-04-21 13:13:22 -0600

    Semi semi terminada

--- Resoluciones/src/Vista/UIBackofficeCoordinador.java
+++ Resoluciones/src/Vista/UIBackofficeCoordinador.java
@@ -108,13 +108,14 @@ public class UIBackofficeCoordinador extends Backoffice implements UIBackoffice{
                 + "<b><font color=\"#CD5C5C\">Reporte de solicitudes atendidas</font></b><br><br>";
         for(DTOSolicitud solicitud : facade.ConsultarSolicitudesAtendidas(desde, hasta)){
             DTOCurso curso = facade.ConsultarCurso(solicitud.getId());
-            //DTOReso
-            reporte += "<b><font color=\"#FA8072\">ID&nbsp;</font></b>" + solicitud.getId() + "<br>";
-            reporte += "<b>Período: <b> " + solicitud.getId() + "<br>";
-            reporte += "<b>Tipo de inconsistencia: <b> " + solicitud.getInconsistencia()+ "<br>";
-            //reporte += "<b>Número de resolución: <b> " + String.valueOf() + "<br>";
-            reporte += "<b>Curso: <b> " + curso.getId() + "  " + curso.getNombre() + "<br>";
-            reporte += "<b>Grupo: <b> " + String.valueOf(solicitud.getnGrupo()) + "<br><br>";
+            DTOResolucion resolucion = facade.ConsultarResolucion(solicitud.getId());
+            reporte+="<b><font color=\"#FA8072\">#Solicitud     </font></b>"+solicitud.getId()+"<br>";
+            reporte+="<b>Período:     <b> "+solicitud.getPeriodo()+"<br>";
+            reporte+="<b>Tipo de inconsistencia:     <b> "+solicitud.getInconsistencia()+"<br>";
+            reporte+="<b>Número de resolución:     <b> "+
+                    (resolucion != null ? String.valueOf(resolucion.getIdResolucion()) : "NO POSEE" )+"<br>";
+            reporte+="<b>Curso:       <b> "+curso.getId()+"  "+curso.getNombre()+"<br>";
+            reporte+="<b>Grupo:       <b> "+String.valueOf(solicitud.getnGrupo())+"<br><br>";
         } return reporte;
     }
     
